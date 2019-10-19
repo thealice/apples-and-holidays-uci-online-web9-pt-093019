93128562253f603036c62135582c3ff0b2e7fcc3
Alice Freda <alicefreda@gmail.com>
2019-10-19 18:25:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,10 +59,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season}:"
-    puts holiday.each do |holiday, supply|
-      puts "#{holiday}: #{supply.flatten}"
-    end
+    puts "#{season.capitalize}:"
+      holiday.each do |holiday, supply|
+        puts "#{holiday}: #{supply.flatten}"
+      end
     end
 end
 
