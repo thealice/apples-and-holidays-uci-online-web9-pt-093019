d3dc4cc74e42ddf8f67f2fc37e2095fcf66c68f0
Alice Freda <alicefreda@gmail.com>
2019-10-19 19:01:02 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
   holiday_hash.map do |season, holiday|
     holiday.map do |holiday, supply|
-      holiday if holiday.include?("BBQ")
+      holiday if supply.include?("BBQ")
     end
   end.flatten.compact
 end
