f5a8ebb000a851bebd89c55d0cb429f968363ac6
Alice Freda <alicefreda@gmail.com>
2019-10-19 18:28:25 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
       holiday.each do |holiday, supply|
-        puts "  #{holiday.capitalize}: #{supply.join(",")}"
+        puts "  #{holiday.capitalize}: #{supply.join(", ")}"
       end
     end
 end
