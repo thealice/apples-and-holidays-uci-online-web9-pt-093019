09a9bbee7ebf5ad2ccb2216e0ae3473d322ce833
Alice Freda <alicefreda@gmail.com>
2019-10-19 18:20:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,6 +61,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     "#{season}:
       #{holiday}: "
+      binding.pry
 end
 
 def all_holidays_with_bbq(holiday_hash)
