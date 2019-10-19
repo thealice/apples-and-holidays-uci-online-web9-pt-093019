ba2af700a59a1741b9f776dd12cf3893d678c475
Alice Freda <alicefreda@gmail.com>
2019-10-19 18:20:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday|
+    "#{season}:
+      #{holiday}: "
 end
 
 def all_holidays_with_bbq(holiday_hash)
